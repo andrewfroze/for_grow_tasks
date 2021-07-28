5fa433b37154c0411c4906925545113835796a2e
Andrei Nikiporchyk <andrewfroze@gmail.com>
2021-07-28 20:09:25 +0300

    box for numbers

---
+++ src/com/training/an/grow/BoxForNumbers/Box.java
@@ -0,0 +1,17 @@
+package com.training.an.grow.BoxForNumbers;
+
+public class Box<T extends Number> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
