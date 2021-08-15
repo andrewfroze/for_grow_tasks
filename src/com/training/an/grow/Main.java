e320a2a9f03a33ae055abfe3704aead5ceb7f246
Andrei Nikiporchyk <andrewfroze@gmail.com>
2021-08-15 21:05:19 +0300

    Singleton

--- src/com/training/an/grow/Main.java
+++ src/com/training/an/grow/Main.java
@@ -1,14 +1,9 @@
 package com.training.an.grow;
 
-import com.training.an.grow.Exceptions.CustomCheckedException;
-import com.training.an.grow.Exceptions.CustomRuntimeException;
-import com.training.an.grow.ScreenshotMaker.ScreenshotMaker;
 
 public class Main {
 
     public static void main(String[] args) {
-        //throw new CustomCheckedException("exception message");
-	    //throw new CustomRuntimeException("exception message");
-        ScreenshotMaker.takeScreenshot("target/screenshots");
+
     }
 }
